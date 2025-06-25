8b440034ff1a389bb90f70a422d27099bb5c8c84
R <rivki871@gmail.com>
2025-06-25 20:49:14 +0300

    push project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,16 +13,16 @@ export class AppComponent implements OnInit {
   title = 'multiplication-game';
   selectedBackground = '';
   private images = [
-    'assets/1.jpg',
-    'assets/2.jpg',
-    'assets/3.jpg',
-    'assets/4.jpg',
-    'assets/5.jpg',
-    'assets/6.jpg',
-    'assets/7.jpg',
-    'assets/8.jpg',
-    'assets/9.jpg',
-    'assets/10.jpg',
+    // 'assets/1.jpg',
+    // 'assets/2.jpg',
+    // 'assets/3.jpg',
+    // 'assets/4.jpg',
+    // 'assets/5.jpg',
+    // 'assets/6.jpg',
+    // 'assets/7.jpg',
+    // 'assets/8.jpg',
+    // 'assets/9.jpg',
+    // 'assets/10.jpg',
   ];
 
   constructor(@Inject(DOCUMENT) private document: Document) { }
